2fec4c2bbb5a1ca9ac194ba2fe07185cee02ab8f
YasinDehfuli <ydehfuli@gmail.com>
2024-08-22 00:08:27 +0330

    feat(school): added school class constructor properties

---
+++ School/school.js
@@ -0,0 +1,19 @@
+class School {
+  constructor(name, address, street, city, state, postalCode, country, yearFounded, area, type, email, website, budget, motto, investor) {
+    this.name = name;
+    this.address = address;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.country = country;
+    this.yearFounded = yearFounded;
+    this.area = area;
+    this.type = type;
+    this.email = email;
+    this.website = website;
+    this.budget = budget;
+    this.motto = motto;
+    this.investor = investor;
+  }
+}
