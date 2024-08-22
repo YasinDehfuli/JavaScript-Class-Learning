809762660ae491e6b6229aecde1b26f4568380f0
YasinDehfuli <ydehfuli@gmail.com>
2024-08-22 22:57:52 +0330

    feat(vice-principal): added vicePrincipalStaff.js, & fix principalStaff.js name class

--- School/principalStaff.js
+++ School/principalStaff.js
@@ -1,4 +1,4 @@
-class PrincipalStaff extends staffs {
+class principalStaff extends staffs {
   constructor(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role, leadershipStyle, parentMeetingsSchedule, adjustAnnualBudget, evaluateTeacherPerformance, schoolVision, schoolPolicies, teacherProfessionalDevelopment, studentDisciplinaryActions, communityOutreachPrograms, infrastructureProjects, emergencyPlans, academicPerformanceReports, teacherHiringPlan) {
     super(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role);
     
