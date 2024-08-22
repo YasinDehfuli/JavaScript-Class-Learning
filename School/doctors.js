fe698ba4610adbf786bed2eaa3e97239cb1d17da
YasinDehfuli <ydehfuli@gmail.com>
2024-08-22 23:07:41 +0330

    feat(doctors): added doctors.js superclass

---
+++ School/doctors.js
@@ -0,0 +1,8 @@
+class doctors extends staffs {
+  constructor(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role, medicalCode, duty,) {
+    super(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role);
+    
+    this.medicalCode = medicalCode;
+    this.duty = duty;
+  }
+}
