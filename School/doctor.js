1fd700e1c70677d50e7d55586f716c57b1ca0ede
YasinDehfuli <ydehfuli@gmail.com>
2024-08-22 23:34:24 +0330

    feat(doctor): added doctor.js extended class from doctors.js

---
+++ School/doctor.js
@@ -0,0 +1,18 @@
+class doctor extends doctors {
+  constructor(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role, medicalCode, duty, medicalRecords, dailyCheckups, emergencyCare, chronicConditions, immunizationRecords, healthEducation, medicationAdministration, healthScreenings, referrals, parentConsultations, staffHealthManagement, injuryReports,) {
+    super(name, family, fatherName, motherName, count, list, id, subject, yearOfExperience, email, phone, age, marriage, insurance, workingHours, department, role, medicalCode, duty,);
+    
+    this.medicalRecords = medicalRecords;
+    this.dailyCheckups = dailyCheckups;
+    this.emergencyCare = emergencyCare;
+    this.chronicConditions = chronicConditions;
+    this.immunizationRecords = immunizationRecords;
+    this.healthEducation = healthEducation;
+    this.medicationAdministration = medicationAdministration;
+    this.healthScreenings = healthScreenings;
+    this.referrals = referrals;
+    this.parentConsultations = parentConsultations;
+    this.staffHealthManagement = staffHealthManagement;
+    this.injuryReports = injuryReports;
+  }
+}
